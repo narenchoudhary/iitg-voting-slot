b98f42c9e9c617fe33c56b14be6a6acee950c93d
Narendra Choudhary <narendra.prog@gmail.com>
2017-02-15 17:09:24 +0530

    Add root pattern for login

--- votingslot/app/urls.py
+++ votingslot/app/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.LoginView.as_view(), name='login'),
     url(r'^login/$', views.LoginView.as_view(), name='login'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
     url(r'^token/$', views.TokenView.as_view(), name='token'),
