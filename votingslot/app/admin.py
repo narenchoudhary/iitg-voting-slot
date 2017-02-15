c7165b1738e434b14447e8ddc9643604073247b1
Narendra Choudhary <narendra.prog@gmail.com>
2017-02-15 17:07:16 +0530

    Update list_display in ModelAdmin classes

--- votingslot/app/admin.py
+++ votingslot/app/admin.py
@@ -4,9 +4,17 @@ from models import Appointment, Slot, Student
 admin.site.site_header = 'Slot Booking Portal'
 
 
-class SlotModel(admin.ModelAdmin):
+class SlotAdmin(admin.ModelAdmin):
     list_display = ['start_time', 'end_time', 'stud_count', 'max_limit']
 
-admin.site.register(Appointment)
-admin.site.register(Slot, SlotModel)
-admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'web_mail', 'department', 'token_booked']
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['slot', 'student', 'token', 'created_on']
+
+admin.site.register(Appointment, AppointmentAdmin)
+admin.site.register(Slot, SlotAdmin)
+admin.site.register(Student, StudentAdmin)
